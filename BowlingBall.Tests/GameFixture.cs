95cb9eb40a6cc8aae7df71b5e16cc6f4608635fd
tavisca-brai <brai@tavisca.in>
2019-07-07 12:27:04 +0530

    Updated the solution and the test cases

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -5,10 +5,70 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
+        private Game _game; 
+        public GameFixture()
+        {
+            _game = new Game();
+        }
+
         [Fact]
         public void DummyTest()
         {
             // This is a dummy test that will always pass.
+        }
+
+        [Fact]
+        public void AllZerosTest()
+        {
+            RollNTImes(0, 20);
+            int score = _game.GetScore();
+
+            Assert.Equal(0, score);
+        }
+
+        [Fact]
+        public void AllOnesTest()
+        {
+            RollNTImes(1, 20);
+            int score = _game.GetScore();
+
+            Assert.Equal(20, score);
+        }
+
+        [Fact]
+        public void OneSpareTest()
+        {
+            _game.Roll(4);
+            _game.Roll(6);
+            _game.Roll(4);
+            RollNTImes(0, 17);
+
+            Assert.Equal(18, _game.GetScore());
+        }
+
+        [Fact]
+        public void OneStrikeTest()
+        {
+            _game.Roll(10);
+            _game.Roll(5);
+            _game.Roll(4);
+            RollNTImes(0, 16);
+
+            Assert.Equal(28, _game.GetScore());
+        }
+
+        [Fact]
+        public void AllStrikeTest()
+        {
+            RollNTImes(10, 12);
+
+            Assert.Equal(300, _game.GetScore());
+        }
+
+        private void RollNTImes(int pins, int times)
+        {
+            for (int i = 0; i < times; i++)
+                _game.Roll(pins);
         }
     }
 }
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,16 +4,66 @@ namespace BowlingBall
 {
     public class Game
     {
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
+        
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            rolls[currentRoll++] = pins;
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int score = 0;
+            int index = 0;
+            for(int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(index))
+                {
+                    score += 10 + StrikeBonus(index);
+                    index++;
+                }else if (IsSpare(index))
+                {
+                    score += 10 + SpareBonus(index);
+                    index += 2;
+                }
+                else
+                {
+                    score += SumOfRolls(index);
+                    index += 2;
+                }
+
+                
+            }
+
+            return score;
         }
 
+        private bool IsSpare(int index)
+        {
+            if (rolls[index] + rolls[index + 1] == 10)
+                return true;
+            else
+                return false;
+        }
+        private int SpareBonus(int index)
+        {
+            return rolls[index + 2];
+        }
+        private bool IsStrike(int index)
+        {
+            if (rolls[index] == 10)
+                return true;
+            else
+                return false;
+        }
+        private int StrikeBonus(int index)
+        {
+            return rolls[index + 1] + rolls[index + 2];
+        }
+        private int SumOfRolls(int index)
+        {
+            return rolls[index] + rolls[index + 1];
+        }
     }
 }
-
